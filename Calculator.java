512d5390a7c7a579f2661d0bbf469920a0d8cf37
Aishwarya <aishwaryaggarwal@gmail.com>
2025-10-03 22:28:02 +0530

    Update Calculator.java
    added mul and div

--- Calculator.java
+++ Calculator.java
@@ -8,9 +8,20 @@ public class Calculator {
         return a - b;
     }
 
+    public int mul(int a, int b) {
+    return a * b;
+    }
+
+   public int div(int a, int b) {
+    if (b != 0) return a / b;
+    else throw new IllegalArgumentException("Cannot divide by zero");
+    }
+
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("Add: " + calc.add(10, 5));
         System.out.println("Sub: " + calc.sub(10, 5));
+        System.out.println("Div: " + calc.div(10,5));
+        System.out.println("Mul: " + calc.mul(10,5));
     }
 }
